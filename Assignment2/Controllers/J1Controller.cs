59a2e0e45a00d049f0673f2d7378bf0656c0457d
JALEEL <JALEEL@DESKTOP-3CMH6R0>
2023-02-27 22:20:10 -0500

    Add project files.

---
+++ Assignment2/Controllers/J1Controller.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Web.Http;
+
+namespace Assignment2.Controllers
+{
+    public class J1Controller : ApiController
+    {
+
+        /*
+        This method takes 4 parameters in integer for a burger, a drink, a side and a dessert respectively and 
+        uses if statements to calculate the calories for each selection, returns the total calories in integer.
+        example: if burger=2, drink=1, side=1, dessert=2, The total = 927
+        */
+        [HttpGet]
+        [Route("api/J1/Menu/{burger}/{drink}/{side}/{dessert}")]
+
+        public int Menu(int burger, int drink, int side, int dessert)
+        {
+            //Burgers
+            int Cheeseburger = 461;
+            int Fishburger=431;
+            int Veggieburger = 420;
+
+            //drinks
+            int Softdrink=130;
+            int Orangedrink=160;
+            int Milk=118;
+
+            //sides
+            int Fries = 100; ;
+            int Bakedpotato=57;
+            int Chefsalad=70;
+
+            //desserts
+            int Applepie = 167;
+            int Sundae = 266;
+            int Fruitcup = 75;
+
+            int total = 0;
+
+            //checking for burgers
+
+            if (burger==1)
+            {
+                total = total + Cheeseburger;
+            }
+            else if(burger==2)
+            {
+                total = total + Fishburger;
+            }
+            else if (burger == 3)
+            {
+                total= total + Veggieburger;
+            }
+
+            else if(burger==4)
+            {
+                total = total + 0;
+            }
+
+            // checking for drinks
+
+            if (drink == 1)
+            {
+                total = total + Softdrink;
+            }
+            else if (drink == 2)
+            {
+                total = total + Orangedrink;
+            }
+            else if (drink == 3)
+            {
+                total = total + Milk;
+            }
+
+            else if (drink == 4)
+            {
+                total = total + 0;
+            }
+
+
+            //checking for sides
+
+            if (side == 1)
+            {
+                total = total + Fries;
+            }
+            else if (side == 2)
+            {
+                total = total + Bakedpotato;
+            }
+            else if (side == 3)
+            {
+                total = total + Chefsalad;
+            }
+            else if (side == 4)
+            {
+                total = total + 0;
+            }
+
+            //checking for dessert
+
+            if (dessert == 1)
+            {
+                total = total + Applepie;
+            }
+            else if (dessert == 2)
+            {
+                total = total + Sundae;
+            }
+            else if (dessert == 3)
+            {
+                total = total + Fruitcup;
+            }
+
+            else if (dessert == 4)
+            {
+                total = total + 0;
+            }
+
+            //returning total
+            return total;
+        }
+    }
+}
---
+++ Assignment2/Controllers/J2Controller.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment2.Controllers
+{
+    public class J2Controller : ApiController
+    {
+        /*
+         This method takes two parameters in integers where m is the number of sides of Dice 1 and n the number
+        of sides of Dice 2. It returns the number of ways two dice can return the sum of 10.
+        example: if m = 15, n=20, then there are 11 ways to get sum of 10
+         */
+        [HttpGet]
+        [Route("api/J2/DiceGame/{m}/{n}")]
+        public string DiceGame(int m, int n)
+        {
+            int counter = 0;
+
+            /* using nested loops to pair each value of dice 1 to values of dice 2 and an if statement to check if the pair returns
+             value 10 */
+            for (int i=0; i<=m; i++) 
+            { 
+                for (int j=0; j<=n; j++) 
+                {
+                    if (i + j == 10)
+                    {
+                        counter++;
+                    }
+                }
+            }
+            return "There are " + counter.ToString() + " total ways to get the sum 10 ";
+        }
+    }
+}
